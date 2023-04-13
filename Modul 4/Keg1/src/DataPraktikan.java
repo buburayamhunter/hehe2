f87fac7d5c776bf7c07467a1dad33ed2322838e0
buburayamhunter <102528120+buburayamhunter@users.noreply.github.com>
2023-04-13 08:23:31 +0700

    Add files via upload

---
+++ Modul 4/Keg1/src/DataPraktikan.java
@@ -0,0 +1,18 @@
+public class DataPraktikan {
+    String nim, nAsisten;
+
+    public DataPraktikan(String nm, String na){
+        nim = nm;
+        nAsisten = na;
+    }
+
+    boolean tambahData(String nimPraktikan, String namaAsisten){
+
+//        HashMap<String, DataPraktikan> dt = new HashMap<>();
+
+        return nimPraktikan, namaAsisten
+    }
+
+    public static void main(String[] args) {
+    }
+}
